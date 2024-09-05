de71b3aac208a2bfff8840d8cf48dad606b14d0b
DuleBondok <dulebondok@gmail.com>
2024-09-05 05:13:30 +0200

    final commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let github = document.getElementById("github");
+let linkedin = document.getElementById("linkedin");
+
+github.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok");
+})
+
+linkedin.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/dusan-bondokic-9b214318b/");
+})
+
+let battleshipProject = document.getElementById("battleshipProject");
+let toDoListProject = document.getElementById("toDoListProject");
+let weatherAppProject = document.getElementById("weatherAppProject");
+let restaurantPage = document.getElementById("restaurantPage");
+let adminDashboard = document.getElementById("adminDashboard");
+let ticTacToe = document.getElementById("ticTacToe");
+
+
+battleshipProject.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/battleship-game");
+})
+
+
+toDoListProject.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/to-do-list");
+})
+
+weatherAppProject.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/weather-app");
+})
+
+restaurantPage.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/restaurant-page");
+})
+
+adminDashboard.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/admin-dashboard");
+})
+
+ticTacToe.addEventListener("click", () => {
+    window.open("https://github.com/DuleBondok/js-tic-tac-toe");
+})
+
+
+
